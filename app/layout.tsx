c1953447ae6d3c207bb8eac9db3b37098810d5e7
Filip <jarolimfilip07@gmail.com>
2025-01-26 20:54:07 +0100

    bug fix

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,12 +5,12 @@ import { SessionProvider } from "next-auth/react"
 import { auth } from "@/auth"
 
 export const metadata: Metadata = {
-    title: 'My Next.js PWA',
+    title: 'Strakatá turistika',
     description: 'A Progressive Web App built with Next.js',
     viewport: 'width=device-width, initial-scale=1',
     icons: [
         { rel: 'apple-touch-icon', url: '/icons/icon-192x192.png' },
-        { rel: 'icon', url: '/favicon.ico' },
+        { rel: 'icon', url: '/icons/icon-192x192.png', type: 'image/png', sizes: '32x32'}
     ],
     themeColor: '#000000',
 }
--- components/structure/Header.tsx
+++ components/structure/Header.tsx
@@ -1,11 +1,15 @@
 "use client"
 
 import React from 'react'
+
+import Image from 'next/image'
+
 import {useCurrentUser} from "@/hooks/use-current-user";
 import {UserButton} from "@/components/auth/user-button";
 import {Navbar} from "@/app/(protected)/_components/navbar";
 import {LoginButton} from "@/components/auth/login-button";
 import {LogoutButton} from "@/components/auth/logout-button";
+import PageLogo from "@/public/icons/icon-192x192.png"
 
 const Header = () => {
 
@@ -13,17 +17,26 @@ const Header = () => {
 
 
     return (
-        <header className={"flex flex-row items-center justify-between w-full p-2"}>
+        <header className={"grid grid-cols-7 w-full p-2"}>
 
-            <UserButton/>
-            <Navbar />
-            <div className={"flex flex-row items-center justify-center"}>
-                <LoginButton>
-                    Join us
-                </LoginButton>
-                <LogoutButton>
-                    Logout
-                </LogoutButton>
+            <div className={"flex flex-row items-center justify-start gap-x-2 font-bold"}>
+                <Image src={PageLogo} alt={"Logo"} width={32} height={32} className={"rounded-full"}/>
+                Strakatá turistika
+            </div>
+            <div className={"col-span-5 w-full flex flex-row items-center justify-center"}>
+                <Navbar />
+            </div>
+            <div className={"flex flex-row items-center justify-end"}>
+                {
+                    !user ?
+                        <LoginButton>
+                            Join us
+                        </LoginButton>
+                        :
+                        <div className={"flex flex-row items-center justify-center gap-x-2"}>
+                            <UserButton/>
+                        </div>
+                }
             </div>
 
         </header>
