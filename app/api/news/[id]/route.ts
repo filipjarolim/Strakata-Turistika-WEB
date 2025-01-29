02cbc06fdfc3391625c713b5b9ab91f6cb125823
Filip <jarolimfilip07@gmail.com>
2025-01-29 17:58:25 +0100

    news database

--- app/api/news/[id]/route.ts
+++ app/api/news/[id]/route.ts
@@ -1,20 +1,25 @@
 import {db} from "@/lib/db";
 import {NextResponse} from "next/server";
 
-export async function PUT(req: Request, { params }: { params: { id: string } }) {
+type tParams = Promise<{ id: string }>;
+
+export async function PUT(req: Request, { params }: { params: tParams }) {
+    const { id } = await params;
     const { title, content } = await req.json();
 
     const news = await db.news.update({
-        where: { id: params.id },
+        where: { id },
         data: { title, content },
     });
 
     return NextResponse.json(news);
 }
 
-export async function DELETE(req: Request, { params }: { params: { id: string } }) {
+export async function DELETE(req: Request, { params }: { params: tParams }) {
+    const { id } = await params;
+
     await db.news.delete({
-        where: { id: params.id },
+        where: { id },
     });
 
     return NextResponse.json({ message: "Deleted successfully" });
